b8a0d1244636215b92971e891dee462d0a82d57c
remansharma <remansharma27@gmail.com>
2024-05-08 21:33:15 +0530

    formatting

--- create-excel-file/index.js
+++ create-excel-file/index.js
@@ -7,21 +7,21 @@ workbook.modified = new Date();
 workbook.lastPrinted = new Date(2016, 9, 27);
 
 // create new sheet with pageSetup settings for A4 - landscape
-const worksheet =  workbook.addWorksheet('My Sheet', {
-  pageSetup:{paperSize: 9, orientation:'landscape'}
+const worksheet = workbook.addWorksheet('My Sheet', {
+    pageSetup: { paperSize: 9, orientation: 'landscape' }
 });
 
 worksheet.columns = [
     { header: 'Id', key: 'id', width: 10 },
     { header: 'Name', key: 'name', width: 32 },
     { header: 'D.O.B.', key: 'DOB', width: 10, outlineLevel: 1 }
-  ];
+];
 
-worksheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
-worksheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
+worksheet.addRow({ id: 1, name: 'John Doe', dob: new Date(1970, 1, 1) });
+worksheet.addRow({ id: 2, name: 'Jane Doe', dob: new Date(1965, 1, 7) });
 
 
-(async()=>{
+(async () => {
     let filename = './MyExcel.csv';
     await workbook.csv.writeFile(filename, {});
 
