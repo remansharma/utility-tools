ef0e46aa21145f902e7079cb11f20817d4b50404
remansharma <remansharma27@gmail.com>
2024-05-25 14:38:50 +0530

    bug fix in mongo-backup.sh

--- take-backup-of-mongodb-inprogress/mongo-backup.sh
+++ take-backup-of-mongodb-inprogress/mongo-backup.sh
@@ -1,7 +1,8 @@
-USERNAME=bhavya
-PASSWORD=
-DATABASE_NAME=test-db
+USERNAME=easyres
+PASSWORD=GPvcYwJ9a9mtERGu
+DATABASE_NAME=dev-inbound
+COLLECTION_NAME=bookings
 CLUSTER_NAME=cluster0.xra9v.mongodb.net
 
 # documentation: https://www.mongodb.com/docs/database-tools/mongodump/
-mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@$CLUSTER_NAME/$DATABASE_NAME --quiet
+mongodump --uri mongodb+srv://$USERNAME:$PASSWORD@$CLUSTER_NAME/$DATABASE_NAME --quiet --collection=$COLLECTION_NAME
